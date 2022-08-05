25ddffb3b01385eb79cd0921093d290f9864ea3a
Jason Baik <baikjs87@gmail.com>
2022-08-05 01:28:03 -0400

    Updated give_away, still WIP received_one?

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,16 +2,17 @@ class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
 
+    # Accepts an item_name (string) and returns true if any of the freebies associated with the dev has that item_name, otherwise returns false
     def received_one?(item)
-        self.freebies.each do |free|
+        freebs = self.freebies
+        freebs.each do |free|
             free.item_name.equal? item
         end
     end
 
     def give_away (dev, freebie)
         if freebie.dev_id == self.id
-            binding.pry
-            freebie.dev_id = dev.id
+            freebie.update(dev_id: dev.id)
         end
     end
 end
